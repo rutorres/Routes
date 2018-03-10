e7a752256421e91f8c116a6a422f32bee474d3fb
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-03-10 10:11:26 -0700

    Initial Commit

---
+++ Routes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Routes
+//
+//  Created by Ruth Torres Castillo on 3/10/18.
+//  Copyright © 2018 New Mexico State University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
