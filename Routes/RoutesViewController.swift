4279c0963efeef84fb35d97b912de3f2aa9f3c58
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-03-10 10:50:23 -0700

    Put the icons to each row

--- Routes/RoutesViewController.swift
+++ Routes/RoutesViewController.swift
@@ -42,10 +42,13 @@ class RoutesViewController: UITableViewController, XMLParserDelegate {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "routeCell", for: indexPath)
+        
         let route = routes[indexPath.row]
         
         cell.textLabel?.text = route.routeName
         cell.detailTextLabel?.text = route.routeColor
+        let imageName = UIImage(named: route.routeColor)
+        cell.imageView?.image = imageName
         
         return cell
     }
